a0dea4e06646da3f55686233243bc5fc1f8c68b2
bastien.marques <bastien.marques@iut-rodez.fr>
2019-11-13 12:11:38 +0100

    ajout du port

--- all_users.php
+++ all_users.php
@@ -2,16 +2,17 @@
 <HTML lang="fr">
 	<HEAD>
 		<title>Premiers pas avec PDO</title>
-		<meta charset="UTF-8">		
+		<meta charset="UTF-8">
 	</HEAD>
 	<BODY>
 			<?php 
 				$host = 'localhost';
+				$port = '3306';
 				$db   = 'my_activities';
 				$user = 'root';
 				$pass = 'root';
 				$charset = 'utf8mb4';
-				$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+				$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 				$options = [
     				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -26,7 +27,7 @@
 			?>
 		<table>
 			<?php   
-				$stmt = $pdo->query('SELECT * FROM users U JOIN status S ON S.id = U.status_id ORDER BY username	');
+				$stmt = $pdo->query('SELECT * FROM users U JOIN status S ON S.id = U.status_id ORDER BY username');
 				echo '<tr><th>Id</th><th>Username</th><th>email</th><th>Status</th></tr>';
 				while ($row = $stmt->fetch())
 				{				
