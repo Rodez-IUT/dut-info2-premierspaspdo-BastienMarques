e01da3179634e1f74eae35467f804a5e4c9e904f
bastien.marques <bastien.marques@iut-rodez.fr>
2019-11-27 11:38:56 +0100

    fix #4 Utilisation d’énoncés préparés

--- all_users.php
+++ all_users.php
@@ -35,7 +35,7 @@ try {
 } catch (PDOException $e) {
     echo $e->getMessage();
     throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+}	
 function get($name) {
     return isset($_GET[$name]) ? $_GET[$name] : null;
 }
@@ -55,10 +55,12 @@ function get($name) {
 </form>
 
 <?php
-$start_letter = htmlspecialchars(get("start_letter"));
+$start_letter = htmlspecialchars(get("start_letter").'%');
 $status_id = (int)get("status_id");
-$sql = "select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id where username like '$start_letter%' and status_id = $status_id order by username";
-$stmt = $pdo->query($sql);
+$sql = "select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id where username like :start_letter and status_id = :status_id order by username";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['start_letter' => $start_letter, 'status_id' => $status_id]);
+
 ?>
 <table>
     <tr>
